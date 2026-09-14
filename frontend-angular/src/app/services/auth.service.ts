46f5e9d24ab155be06f9ff84ab0b12ae435cbc3b
Rishabh Prajapati <prajapatirishabh071@gmail.com>
2026-09-14 16:59:51 +0530

    feat: add auth, chat, and vault services

--- frontend-angular/src/app/services/auth.service.ts
+++ frontend-angular/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap, of } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 export interface User {
   id: string;
--- frontend-angular/src/app/services/chat.service.ts
+++ frontend-angular/src/app/services/chat.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject, of } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 export interface ThreadSummary {
   id: string;
